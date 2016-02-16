02fbd99fbd6e483bf8b8dc50b8a4b08e68a077a8
santi15161 <santi15161@gmail.com>
2016-02-16 11:52:47 -0500

    mcd

---
+++ primes.py
@@ -0,0 +1,26 @@
+
+# x = input("Primer Numero: ")
+# y = input("Segundo Numero: ")
+#
+#
+# if x<y:
+#	print x
+# else:
+#	print y
+#
+#
+
+
+def minimo (x,y):
+	if x < y:
+		return x
+	else:
+		return y
+def mcd (x,y):
+	m = minimo (x,y)
+	for i in range (m,0,-1):
+
+		if x % i == 0 and y % i == 0
+			return i
+		else 
+			return 
