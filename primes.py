b40902376eab4c808a7fb8c1f52580070f18278f
santi15161 <santi15161@gmail.com>
2016-02-18 11:33:09 -0500

    Actualizacion

--- primes.py
+++ primes.py
@@ -10,6 +10,7 @@
 #
 #
 
+# esta funcion define  si un numero es menor que otro 
 
 def minimo (x,y):
 	if x < y:
@@ -20,7 +21,35 @@ def mcd (x,y):
 	m = minimo (x,y)
 	for i in range (m,0,-1):
 
-		if x % i == 0 and y % i == 0
+		if x % i == 0 and y % i == 0:
 			return i
-		else 
-			return 
+
+# Esta funcion define si un numero es coprimo o no 
+
+def arecoprime (a,b):
+
+	m = mcd (a,b)
+
+	if m==1:
+		return 1
+	else:
+		return 0
+
+print arecoprime (12,16)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
