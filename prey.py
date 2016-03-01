3267fa14069866d67706297c5942092f7b5d735c
santi15161 <santi15161@gmail.com>
2016-03-01 11:35:18 -0500

    Actualizacion drug

--- prey.py
+++ prey.py
@@ -10,11 +10,26 @@ import matplotlib.pyplot
 # Aca definimos las variables
 
 
+ky = 2.0
+kx = 1.06
+kyx =0.01
+kxy = 0.01
+y0 = 100
+x0 = 15 
+d = 0.1
+x = range(100)
+y = range(100)
 
 # Aca indicamos el proceso a realizar Y luego lo imprimimos
 
-for i in range ():
+for i in range (100):	
+
+	y1 = y0 - ky * D * y0
+	y0 = y1
+	x[i] = y1
+	y[i] = (i * 0.1)
 	
+
 xnew = numpy.array (x)
 ynew = numpy.array (y)
 
