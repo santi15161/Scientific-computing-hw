1afdbc950dcdc510e6d5e235e468e15407418373
santi15161 <santi15161@gmail.com>
2016-03-31 11:38:46 -0500

    Clase 31 de marzo

--- neuron.py
+++ neuron.py
@@ -8,39 +8,68 @@ import matplotlib.pyplot as plt
 import sys
 
 T = 6.3
-Phi = 3**(T-6.3)/10, 1 in projects
-Delta = 0.001
+phi = 3**(T-6.3)/10
+delta = 0.001
 V = -65
 
+n= 0.317
+m= 0.05
+h= 0.6
+
+gK = 36
+gNa = 120
+gL = 0.3
+
+VK= -77
+VNa= 50
+VL= -54.4
+
+Cm = 0.1
+I = 15
+Ik = gK** (n**4) * (V-VK)
+IL = gL*(V-VL)
+INa = gNa * (m**3) * h * (V-VNa)
 
 
 
-An = phi(0.01(V + 10)/(exp((V + 10)/10)-1))
-AM = phi(0.01(V + 25)/(exp((V + 25)/10)-1))
-AH = phi(0.07 exp (V/20))
-BN = phi(0.125 exp (V/80))
-BM = phi(4 exp (V/18))
-BH = phi(1/(exp((V + 30)/10)+1))
+Alpha_n = phi * (0.01 * (V + 10)/(np.exp((V + 10)/10)-1))
+Alpha_M = phi * (0.01 * (V + 25)/(np.exp((V + 25)/10)-1))
+Alpha_H = phi * (0.07 * np.exp(V/20))
+Beta_N = phi * (0.125 * np.exp (V/80))
+Beta_M = phi * (4 * np.exp(V/18))
+Beta_H = phi * (1/(np.exp((V + 30)/10)+1))
+
+
+
+def f1(asd1):
+	asd1 = Alpha_n*(1-n)-Beta_n*n
+	return  asd1
+
+def f2(asd2):
+	asd2 = Alpha_m*(1-m)-Beta_m*m
+	return asd2
+
+def f3(asd3):
+	asd3 = Alpha_h*(1-h)- Beta_h*h
+	return asd3
 
 
 
 
 
 
+# = (I-gK**(n**4)(V-VK)- gNa*m**3*h*(V-VNa)-gL(V-VL))/Cm
 
 
 
-
-
-
-from scipy.interpolate import interpold
-
-# generates the data
-
-x = np.linspace (0,10,num=10,endpoint=True)
-y = np.cos(-x**2 / 9.0)
-f = interpld (x,y,kind="Cubic")
-
-# evaluates
-print f(0.2)
+#  from scipy.interpolate import interp1d
+#
+#  # generates the data
+#
+#  x = np.linspace (0,10,num=10,endpoint=True)
+#  y = np.cos(-x**2 / 9.0)
+#  f = interp1d (x,y,kind="Cubic")
+#
+#  # evaluates
+#  print f(0.2)
 
