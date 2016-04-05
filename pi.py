bbd7d3459bf1e2f8044f437fcbfc28ac6bdeec6b
santi15161 <santi15161@gmail.com>
2016-04-05 11:35:39 -0500

    Clase 5 de abril

---
+++ pi.py
@@ -0,0 +1,40 @@
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+import sys
+
+np.random.seed(0)
+
+for i in range(10):
+
+	print np.random.random()
+
+
+
+
+
+
+
+
+
+
+
+R = 1
+n =  np.sqrt(Y**2 + X**2) < R
+
+N0 = 100
+for i in range(N0)
+	X = 
+	Y = 
+	print
+
+N1 = 1000
+for i in range(N1)
+	
+	print
+
+N2 = 10000
+for i in range(N2)
+	
+	print
+
