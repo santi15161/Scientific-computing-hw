df3b2cc09171c0cda64f06930d81ee031431dcbe
santi15161 <santi15161@gmail.com>
2016-04-14 11:36:53 -0500

    Actualizacion clase 14 de abril

--- rwalk.py
+++ rwalk.py
@@ -2,39 +2,35 @@ import numpy as np
 import matplotlib as mplt
 import matplotlib . pyplot as plt
 
+n= 100
+N =1000
+q=0.5,0.7,0.3
+
 def rwalk1d (N , p ):
 
 	s = 0
-
-	for i in range ( N ):
-			
-		r = np . random . random ()
-			
-		if r > p :
-			
-			s = s + 1
-			
+	for i in range ( N ):			
+		r = np.random.random ()			
+		if r > p :			
+			s = s + 1			
 		else :
-			
 			s = s - 1
-			
 	return s
 
-n= 100
-N = 1000
-q=0.5,0.7,0.3
-
-# Aca error 
-
-for i in range(n):
-
-	n0= N/50
-	n1 = n0 + i
-	n = i
 
 
-	print n, np.random.random()
+def f1(N):
+
+	for i in range (100):	
+		
+		x=rwalk1d(N)
+
+		print x
+
+	return f1(N)
+
+#		n= i
+#		ad = N/np.random.random () 
 
 
 
-	
