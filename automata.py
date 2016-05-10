29dd1f3a2f45097ff656d635a523e9976e611398
santi15161 <santi15161@gmail.com>
2016-05-10 11:34:46 -0500

    Actualizacion 10 mayo

--- automata.py
+++ automata.py
@@ -8,41 +8,56 @@ n = 100
 np.zeros(0)
 w = np.zeros([n, n], dtype = np.int8)
 
+i = n/2 
+j = n/2
+d = 0
+
+east = 0
+south = 1
+west = 2
+north = 3
+
+blanco = 0
+negro = 1
+
 def nextDirection (d, c):
 	
-	if d == 0:
-		c = east 
-		print east 
+	if c == blanco:
 
-	if d == 1:
-		c = south 
-		print south 
+		if d == east:
+			d1 = south
+		
+		if d == south:
+			d1 = west
+		
+		if d == west:
+			d1 = north
 
-	if d == 2:
-		c = west 
-		print west
+		if d == north:
+			d1 = east
 
-	if d == 3:
-		c = north 
-		print north 
+	if c == negro:
+	
+		if d == east:
+			d1 = north
+		
+		if d == south:
+			d1 = east
+		
+		if d == west:
+			d1 = south
 
-	return d
+		if d == north:
+			d1 = west
 
-def forward (i, j, d):
+	return d1
+
+def moveforward (i, j, d):
 	
 	return 
 
 def validindex (i, j, n):
-
+		# Validar si la posicion sirve o no 
 	return 
-
-i = n/2 
-d = 0
-
-	
-for i in range(20000):
-
-	print 
-
 
 plt.imshow(w, interpolation='none', cmap= plt.cm.Greys, extent=(0,n,0,n))
